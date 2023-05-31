d39602c539b0e96d5438351d522585199b8352dc
KrysDev <webdevkrys@gmail.com>
2023-05-31 22:08:06 +0100

    x

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-import { User } from './models/User';
+// import { User } from './models/User';
 
-const user = new User({ name: 'NAME XXX', age: 20 });
+// const user = new User({name: 'XXX', age: 99}); // UserProps interface allows to have the properties optional
 
-console.log(user.get('name'));
+// // user.set({ name: 'ABC' });
 
+// // console.log(user.get('name'));
+// // console.log(user.get('age'));
+
+// user.on('click', () => {});
+// console.log(user)
+
+import axios from 'axios';
+
+// axios.post('http://127.0.0.1:3005/users', {
+//   name: 'XXXXX',
+//   age: 99,
+// });
+
+axios.get('http://localhost:3005/users');
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,20 @@
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string; // optional property
+  age?: number; // optional property
 }
 
+type Callback = () => void;
+
 export class User {
+  // 'events' is an array of different callback functions registered for each event (we iterate through this array and call each of the callbacks)
+  // 'key' is a name of an event (click, hover etc.) and it has to be a string
+  // <  this.events['click'] = [ ()=>{} ]  >
+  events: { [key: string]: Callback[] } = {}; // initialize as an empty object
+
   // 'data' is an object
   constructor(private data: UserProps) {}
 
-  // propName (name/age)
+  // propName is a string of: 'name' or 'age'
   get(propName: string): number | string {
     return this.data[propName];
   }
@@ -16,5 +23,28 @@ export class User {
   set(update: UserProps): void {
     // overwrite the 'data' properties with the 'update' properties
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    // if there is no event handler when we create a 'User' (it's undefined), then we create an empty array
+    const handlers = this.events[eventName] || []; // either way 'handlers' is an array
+    // add a new callback function to the array
+    handlers.push(callback);
+    // assign the handlers to the object
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    // check if 'handlers' is defined
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    // else - go through the array and call each function
+    handlers.forEach((callback) => {
+      callback();
+    });
   }
 }
