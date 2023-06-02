5441fee6c52f46e45ccb8443f679486cea6d6e9b
KrysDev <webdevkrys@gmail.com>
2023-06-02 18:03:26 +0100

    x

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,13 +1,19 @@
-// keys of an object are strings, strings can be types, so keys can be types in TypeScript
-
 export class Attributes<T extends object> {
   // 'data' is an object
   constructor(private data: T) {}
 
-  // a method to get a User's property
-  // propName is a string of: 'name' or 'age' or 'id'
-  get(propName: string): number | string {
-    return this.data[propName];
+  // We can use generics with class methods too
+  // 'get' is a method to obtain a User's property value
+  // 'key' is a string : 'name' 'age' or 'id'
+  // This is a generics constraint. It limits the type that K can be (limits what the argument 'key' can be)
+  // Keys of an object are strings, strings can be types, so keys can be types in TypeScript (see at the bottom)
+  // <K extends keyof T> So the type of K can only be one of the keys of the object T and it will be a fixed string
+  // (i.e. if T is UserProps then the keys of the object are 'name' 'age' or 'id' )
+  // Whatever argument we pass in as a 'key' it has to be a type of K
+  // It has to return T[K] - it is a normal object look up: object[property]
+  // So look up the interface T and return the value of the key/property at K (for 'id' it will be 'number', for 'name' 'string' etc.)
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   // a method to change the User's property(ies)
@@ -17,3 +23,8 @@ export class Attributes<T extends object> {
     Object.assign(this.data, update);
   }
 }
+
+// Strings can be types so:
+//   type someWord: 'word'
+// and if the type 'someWord' is used, then the value cannot be different than 'word'.
+// Any other thing will throw an error.
